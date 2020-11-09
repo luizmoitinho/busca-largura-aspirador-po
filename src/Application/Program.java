44324563f8b4d9cfaf49dc57c549877eb79974f7
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-11-09 10:18:02 -0300

    feat - preenchimento da matriz de adjacência e metodos para gerenciar a limpeza

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -7,14 +7,45 @@ public class Program {
 
 	public static void main(String[] args) {
 		Graph graph = new Graph();
-		graph.addVertex('B');
-		graph.addVertex('C');
-		graph.addVertex('D');
-		graph.addVertex('G');
-		graph.addVertex('M');
-		graph.addVertex('R');
+		
+		System.out.println("\n Lista de vértices \n");
+		
+		graph.addVertex('0');
+		graph.addVertex('1');
+		graph.addVertex('2');
+		graph.addVertex('3');
+		graph.addVertex('4');
+		graph.addVertex('5');
+		graph.addVertex('6');
+		graph.addVertex('7');
+		
 		
 		graph.showListVertex();
+		graph.addEdge(0, 0); //0 -> 0
+		graph.addEdge(0, 1); //0 -> 1
+		graph.addEdge(0, 2); //0 -> 2
+		
+		graph.addEdge(1, 1); //1 -> 1
+		graph.addEdge(1, 0); //1 -> 0
+		graph.addEdge(1, 5); //1 -> 5
+		
+		graph.addEdge(2, 2); //2 -> 2
+		graph.addEdge(2,3);  //2 -> 3
+
+		graph.addEdge(3, 3); //3 -> 3
+		graph.addEdge(3, 2); //3 -> 2
+		graph.addEdge(3, 7); //3 -> 7
+		
+		graph.addEdge(4, 4); //4 -> 4
+		graph.addEdge(4, 3); //4 -> 3
+		graph.addEdge(4, 6); //4 -> 6
+		
+		graph.addEdge(5, 5); //5 -> 5
+		graph.addEdge(5, 4); //5 -> 4
+
+		
+		System.out.println("\n Matriz de adjacência \n");
+		graph.showAdjMat();
 		
 		
 	}
--- src/TAD/Graph.java
+++ src/TAD/Graph.java
@@ -4,32 +4,43 @@ public class Graph {
 	
 	protected Vertex[] vertexList = new Vertex[8];
 	protected int[][] adjMat = new int[8][8];
-	private   int nVerts = 0;
+	private int nVerts = 0;
 	
-	
+
 	public void addVertex(char lab) {
 		this.vertexList[this.nVerts++] = new Vertex(lab);
-		
+		this.prepareAdjMat();
 	}
 	
 	public void addEdge(int start, int end) {
-		this.adjMat[start][end] = 1;
-		this.adjMat[end][start] = 1;
+		this.adjMat[start][end] = 1; 
+		this.adjMat[end][start] = 1; 
+		
 	}
 	
 	public void showListVertex() {
 		for(int i = 0;i<this.nVerts;i++) {
 			System.out.println("Index "+i+" - Label: "+this.vertexList[i].label+" | wasVisited: "+this.vertexList[i].wasVisited);
 		}
+		
 	}
 	
 	public void showAdjMat() {
 		for(int i=0;i<this.adjMat.length;i++) {
 			for(int j=0;j<this.adjMat.length;j++) {
-				System.out.print(this.adjMat[i][j]+" ");
+				System.out.printf("%4d",this.adjMat[i][j]);
 			}
 			System.out.println(" ");
 		}
+		
 	}
+	
+	private void prepareAdjMat() {
+		for(int i=0;i<this.adjMat.length;i++)
+			for(int j=0;j<this.adjMat.length;j++) 
+				this.adjMat[i][j] = -1;
+
+	}
+	
 
 }
